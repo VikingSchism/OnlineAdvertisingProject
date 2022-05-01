95a6f608624df7e84daa1563cc35e74db121b6b4
VikingSchism <michael.hopper55@gmail.com>
2022-05-01 15:45:34 +0100

    Fixed ordering of comparison

--- Measurements/comparison.py
+++ Measurements/comparison.py
@@ -7,17 +7,21 @@ dsps = sys.argv[1]
 vals = []
 
 
+with open('watts_' + dsps + 'thread.out') as f:
+    watts = []
+    for line in f:
+        val = json.loads(line[48:])['value']
+        watts.append(val)
+    vals.append(watts)
 with open('watts_' + dsps + '_ssp.out') as f:
     watts = []
     for line in f:
         val = json.loads(line[48:])['value']
         watts.append(val)
     vals.append(watts)
-with open('watts_' + dsps + 'thread.out') as f:
-    for line in f:
-        val = json.loads(line[48:])['value']
-        watts.append(val)
-    vals.append(watts)
 fig, ax = plt.subplots()
 ax.boxplot(vals)
+ax.set_title(sys.argv[1] + " DSPs idling vs receiving requests")
+ax.set_xticks([1,2], ['Idle', 'Requests'])
+ax.set_ylabel("Power (W)")
 plt.show()
