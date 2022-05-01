538c38711b27c01bf77395a92e5c155155a44658
VikingSchism <michael.hopper55@gmail.com>
2022-05-01 13:07:48 +0100

    analyse.py updated to produce boxplots

--- Measurements/analyse.py
+++ Measurements/analyse.py
@@ -13,12 +13,20 @@ for i in range(1,7):
             #        if i % 2 == 0:
             #           amps.append(json.loads(lines[i][48:])['value'])
             #        else:
-            watts.append(json.loads(lines[j][48:])['value'])
+            val = json.loads(lines[j][48:])['value']
+            if val != 0:
+                watts.append(val)
+        print('watts_' + str(i) + 'thread.out')
+        print(np.mean(watts))
     vals.append(watts)
 
-fig, (axs1, axs2) = plt.subplots(2,3)
-for i in range(3):
-    axs1[i].plot(np.arange(len(vals[i])), np.array(vals[i]))
-    axs1[i].plot(np.arange(len(vals[i])), np.repeat(np.average(vals[i]), len(vals[i])))
-    axs2[i].plot(np.arange(len(vals[2+i])), np.array(vals[2+i]))
+fig, ax = plt.subplots()
+ax.boxplot(vals)
+ax.set_xlabel("DSPs connected")
+ax.set_ylabel("Power (W)")
+ax.set_title("Connected DSPs vs idle power consumption")
+#for i in range(3):
+#    axs1[i].plot(np.arange(len(vals[i])), np.array(vals[i]))
+#    axs1[i].plot(np.arange(len(vals[i])), np.repeat(np.average(vals[i]), len(vals[i])))
+#    axs2[i].plot(np.arange(len(vals[2+i])), np.array(vals[2+i]))
 plt.show()
